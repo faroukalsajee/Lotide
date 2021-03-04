d55a9961b9254ae0fb9f70dcfa88dc732cef3831
Farouk Alsajee <42353964+faroukalsajee@users.noreply.github.com>
2021-03-03 17:52:05 -0800

    Add files via upload

---
+++ letterPositions.js
@@ -0,0 +1,48 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    // console.log("Arrays not equal length")
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+      // if it is an array && the arrays are not equal then return false;
+      return false;
+    }
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+      // if the element is not an array and the items are not equal then return false
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (letter === ' ') {
+      continue;
+    } else if (!results[letter]) {
+      results[letter] = [i];
+
+    } else {
+      results[letter].push(i);
+    }
+
+  }
+  return results;
+};
+console.log(letterPositions('I write code'))
+//console.log(countLetters('I am Web Developer'));
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+
+module.exports = letterPositions;
